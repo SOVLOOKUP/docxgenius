722915144eeec2ca6e819919af5d8850a2126f36
fan xia <gonorth@qq.com>
2024-04-01 01:05:00 +0800

    mv

--- test.js
+++
@@ -1,8 +0,0 @@
-
-const { render } = require(".")
-render("C:\\Users\\xiafan\\Desktop\\story.docx",
-    "C:\\Users\\xiafan\\Desktop\\new_story.docx", {
-    storyName: "故事名称",
-    storyAuthor: "凡凡",
-    storySource: "理想"
-})
