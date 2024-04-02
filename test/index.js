9b5a704c7a78f3d954737cd685b8a886addff174
fan xia <gonorth@qq.com>
2024-04-02 14:49:59 +0800

    ok

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 
 import { DocxTemplate } from "../index.js"
 
-const dt = DocxTemplate()
+const dt = new DocxTemplate()
 
 dt.renderFile("C:\\Users\\xiafan\\Desktop\\story.docx", "C:\\Users\\xiafan\\Desktop\\new_story.docx", JSON.stringify(
     {
-        storyName: "故事名称",
-        storyAuthor: "作者",
-        storySource: "来源"
+        story_name: "故事名称",
+        story_author: "作者",
+        story_source: "来源"
     }
 ))
