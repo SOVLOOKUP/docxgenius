e78e4fe1c5f7e5901c6b91f9e9e6c5550915db23
fan xia <gonorth@qq.com>
2024-04-05 12:25:21 +0800

    main

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+mod deps;
+use j4rs::{Jvm, JvmBuilder, MavenArtifactRepo, MavenSettings};
+
+fn main() {
+  let jvm: Jvm = JvmBuilder::new()
+    .with_maven_settings(MavenSettings::new(vec![MavenArtifactRepo::from(
+      "jitpack.io::https://www.jitpack.io",
+    )]))
+    .build()
+    .unwrap();
+
+  deps::deps(&jvm);
+}
